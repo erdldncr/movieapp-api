e1b45b2383f604a3a93f0ab79002b5c6efd94a6b
erdldncr <erdldncr@gmail.com>
2021-06-28 22:06:00 +0100

    ccomplete project

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ var logger = require('morgan');
 const {db}=require('./helpers/db')
 db()
 
+///config
+const config=require('./helpers/config')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
 var directorRouter = require('./routes/director');
 
 var app = express();
+
+app.set('api_secret_key',config.api_secret_key)
+
+//token middleware
+const verifyToken=require('./middlewares/verify-token')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +32,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/movies', moviesRouter);
-app.use('/api/directors', directorRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/verify',verifyToken);
+app.use('/api/verify/movies', moviesRouter);
+app.use('/api/verify/directors', directorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+const  bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+
+//Models
+const UserModel=require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  UserModel.find((err, data) => {
+    if (err) res.json(err);
+    res.json(data);
+  })
 });
+
+router.post('/authenticate', (req, res) => {
+  const {username,password} = req.body;
+  UserModel.findOne({username})
+  .then((data)=>{
+    if(!data){
+    return  res.send('User couldn\'t be found')
+    }else{
+      bcrypt.compare(password,data.password)
+      .then(loginResult=>{
+        if(!loginResult){
+          return res.send("Authentication Failed")
+        }else{
+          const payload={username}
+          const token=jwt.sign(payload,req.app.get('api_secret_key'),{expiresIn:720})///60*12 saat =720
+          return res.status(200).json(token)
+        }
+      })
+    }
+    
+  })
+  .catch((err)=>{
+   return res.json(err)
+  })
+  
+});
+
+
+router.post('/register', (req, res) => {
+  const {username,password} = req.body;
+
+  bcrypt.hash(password, 10, function(err, hash) {
+   
+    const newUser = new UserModel({username,password:hash});
+    newUser.save()
+             .then((data)=>{res.json(data)})
+             .catch((err)=>{res.json(err)})
+    
+    });
+
+});
+
+
 
 module.exports = router;
